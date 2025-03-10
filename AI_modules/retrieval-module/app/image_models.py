6a51ea7cce3d09963ab1f7c44e21104a684cf0b5
wwaihoe <waihoe88@gmail.com>
2025-03-10 14:54:40 +0800

    update image caption model to do OCR first

--- AI_modules/retrieval-module/app/image_models.py
+++ AI_modules/retrieval-module/app/image_models.py
@@ -1,38 +1,58 @@
 from PIL import Image
 from transformers import AutoProcessor, AutoModelForCausalLM 
-
 import torch
-#Use GPU if available
-if torch.cuda.is_available():
-    device = 'cuda'
-else:
-    device = 'cpu'
+
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 
 class ImageCaptionModel():
   def __init__(self, model_str: str="microsoft/Florence-2-base-ft"):
-    self.model = AutoModelForCausalLM.from_pretrained(model_str, trust_remote_code=True)
+    self.model = AutoModelForCausalLM.from_pretrained(model_str, torch_dtype=torch_dtype, trust_remote_code=True).to(device)
     self.processor = AutoProcessor.from_pretrained(model_str, trust_remote_code=True)
+
+  def predict(self, image, task_prompt, text_input=None):
+    if text_input is None:
+        prompt = task_prompt
+    else:
+        prompt = task_prompt + text_input
+    try:
+      inputs = self.processor(text=prompt, images=image, return_tensors="pt").to(device, torch_dtype)
+      generated_ids = self.model.generate(
+        input_ids=inputs["input_ids"].cuda(),
+        pixel_values=inputs["pixel_values"].cuda(),
+        max_new_tokens=1024,
+        early_stopping=False,
+        do_sample=False,
+        num_beams=3,
+      )
+      generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+      parsed_answer = self.processor.post_process_generation(
+          generated_text, 
+          task=task_prompt, 
+          image_size=(image.width, image.height)
+      )
+      return parsed_answer
+
+    except Exception as e:
+      print("Error predicting")
+      print(e)
+      return None
   
   def generate(self, file):
     image = self.load_image(file)
-    prompt = "<MORE_DETAILED_CAPTION>"
-    try:
-      inputs = self.processor(text=prompt, images=image, return_tensors="pt")
-      generated_ids = self.model.generate(
-        input_ids=inputs["input_ids"],
-        pixel_values=inputs["pixel_values"],
-        max_new_tokens=1024,
-        do_sample=False,
-        num_beams=3
-      )
-      generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
-      parsed_answer = self.processor.post_process_generation(generated_text, task="<MORE_DETAILED_CAPTION>", image_size=(image.width, image.height))
-      caption = parsed_answer["<MORE_DETAILED_CAPTION>"]
-      print("Generated caption: ", caption)
-    except Exception as e:
-      print("Error generating caption")
-      print(e)
+    # Use OCR
+    print("Using OCR")
+    ocr_result = self.predict(image, "<OCR>")["<OCR>"]
+    print("OCR result: ", ocr_result)
+    if ocr_result != "":
+      return ocr_result
+
+    # If OCR does not work, generate a detailed caption
+    print("Generating detailed caption")
+    caption = self.predict(image, "<MORE_DETAILED_CAPTION>")["<MORE_DETAILED_CAPION>"]
+    print("Caption: ", caption)
     return caption
   
   def load_image(self, file):
